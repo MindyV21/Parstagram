2af3a36389999c5efb281593db45bd6a6ab69699
Mindy Van <mmv0321@gmail.com>
2021-07-06 17:02:16 -0500

    define post and set up query

--- app/src/main/java/com/codepath/parstagram/MainActivity.java
+++ app/src/main/java/com/codepath/parstagram/MainActivity.java
@@ -6,14 +6,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.codepath.parstagram.databinding.ActivityMainBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
     private Button btnLogout;
 
     @Override
@@ -22,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        etDescription = binding.etDescription;
+        btnCaptureImage = binding.btnCaptureImage;
+        ivPostImage = binding.ivPostImage;
+        btnSubmit = binding.btnSubmit;
 
         // simple logout to test
         btnLogout = binding.btnLogout;
@@ -33,6 +49,29 @@ public class MainActivity extends AppCompatActivity {
                 ParseUser currentUser = ParseUser.getCurrentUser();
                 Toast.makeText(MainActivity.this, "User logged out!", Toast.LENGTH_SHORT).show();
                 finish();
+            }
+        });
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+
+        // get a list of all posts objects from database
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username " + post.getUser().getUsername());
+                }
             }
         });
     }
--- app/src/main/java/com/codepath/parstagram/ParseApplication.java
+++ app/src/main/java/com/codepath/parstagram/ParseApplication.java
@@ -3,11 +3,15 @@ package com.codepath.parstagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
 
         // init parse
         Parse.initialize(new Parse.Configuration.Builder(this)
