a0d83ffb9488ebbd80d647e0b48a785bd8475c88
Mindy Van <mmv0321@gmail.com>
2021-07-06 19:05:58 -0500

    implemented feed of posts

--- app/src/main/java/com/codepath/parstagram/fragments/ComposeFragment.java
+++ app/src/main/java/com/codepath/parstagram/fragments/ComposeFragment.java
@@ -233,25 +233,4 @@ public class ComposeFragment extends Fragment {
             }
         });
     }
-
-    private void queryPosts() {
-        // specify which class to query
-        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
-        query.include(Post.KEY_USER);
-
-        // get a list of all posts objects from database
-        query.findInBackground(new FindCallback<Post>() {
-            @Override
-            public void done(List<Post> posts, ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, "Issue with getting posts", e);
-                    return;
-                }
-
-                for (Post post : posts) {
-                    Log.i(TAG, "Post: " + post.getDescription() + ", username " + post.getUser().getUsername());
-                }
-            }
-        });
-    }
 }
--- app/src/main/java/com/codepath/parstagram/fragments/PostsFragment.java
+++ app/src/main/java/com/codepath/parstagram/fragments/PostsFragment.java
@@ -2,13 +2,28 @@ package com.codepath.parstagram.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.codepath.parstagram.Post;
 import com.codepath.parstagram.R;
+import com.codepath.parstagram.adapters.PostsAdapter;
+import com.codepath.parstagram.databinding.FragmentComposeBinding;
+import com.codepath.parstagram.databinding.FragmentPostsBinding;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +31,13 @@ import com.codepath.parstagram.R;
  * create an instance of this fragment.
  */
 public class PostsFragment extends Fragment {
+
+    public static final String TAG = "PostFragment";
+
+    FragmentPostsBinding binding;
+    private PostsAdapter adapter;
+    private RecyclerView rvPosts;
+    private List<Post> allPosts;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,7 +82,46 @@ public class PostsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentPostsBinding.inflate(getLayoutInflater(), container, false);
+        // layout of fragment is stored in a special property called root
+        View view = binding.getRoot();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posts, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = binding.rvPosts;
+
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+
+        // get a list of all posts objects from database
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
