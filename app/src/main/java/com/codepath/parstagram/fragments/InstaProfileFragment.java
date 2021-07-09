065d85146250fc60dcc0436c26795e280db5c2ac
Mindy Van <mmv0321@gmail.com>
2021-07-09 14:09:17 -0500

    profile dummy data

--- app/src/main/java/com/codepath/parstagram/fragments/InstaProfileFragment.java
+++ app/src/main/java/com/codepath/parstagram/fragments/InstaProfileFragment.java
@@ -1,9 +1,12 @@
 package com.codepath.parstagram.fragments;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -15,12 +18,16 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.codepath.parstagram.R;
 import com.codepath.parstagram.databinding.FragmentInstaProfileBinding;
 import com.codepath.parstagram.databinding.FragmentPostsBinding;
 import com.codepath.parstagram.models.Post;
+import com.codepath.parstagram.models.User;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
@@ -31,7 +38,7 @@ public class InstaProfileFragment extends  Fragment{
     public static final String TAG = "InstaProfileFragment";
     FragmentInstaProfileBinding binding;
 
-    ImageView ivProfile;
+    ImageView ivProfileImage;
     TextView tvPostsNum;
     TextView tvPostsText;
     TextView tvFollowersNum;
@@ -60,7 +67,7 @@ public class InstaProfileFragment extends  Fragment{
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ivProfile = binding.ivProfileImageActual;
+        ivProfileImage = binding.ivProfileImageActual;
         tvPostsNum = binding.tvPostsNum;
         tvPostsText = binding.tvPostsText;
         tvFollowersNum = binding.tvFollowersNum;
@@ -79,6 +86,35 @@ public class InstaProfileFragment extends  Fragment{
     }
 
     private void setUpProfile() {
+        ParseUser user = ParseUser.getCurrentUser();
 
+        ParseFile image = user.getParseFile(User.KEY_PROFILE_IMAGE);
+        if (image != null) {
+            Glide.with(getContext()).load(image.getUrl()).transform(new CircleCrop()).into(ivProfileImage);
+        } else {
+            Drawable drawable = AppCompatResources.getDrawable(getContext(), R.drawable.ic_profile);
+            ivProfileImage.setImageDrawable(drawable);
+        }
+
+        // dummy data
+        tvPostsNum.setText("50");
+        tvPostsNum.setTextColor(Color.BLACK);
+        tvPostsText.setText("Posts");
+        tvPostsText.setTextColor(Color.BLACK);
+
+        tvFollowersNum.setText("100");
+        tvFollowersNum.setTextColor(Color.BLACK);
+        tvFollowersText.setText("Followers");
+        tvFollowersText.setTextColor(Color.BLACK);
+
+        tvFollowingNum.setText("150");
+        tvFollowingNum.setTextColor(Color.BLACK);
+        tvFollowingText.setText("Following");
+        tvFollowingText.setTextColor(Color.BLACK);
+
+        tvProfileName.setText("mindy");
+        tvProfileName.setTextColor(Color.BLACK);
+        tvProfileBody.setText("This is my description!");
+        tvProfileBody.setTextColor(Color.BLACK);
     }
 }
